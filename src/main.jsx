d72ab61f67c910b86a70b10b1e23ea27332631f9
samarth336 <patilsamarth543@gmail.com>
2025-01-14 17:18:16 +0530

    random changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Sample from './Sample.jsx'
-
 createRoot(document.getElementById('root')).render(
   <div className='block maincontainer'>
     <App />
-    {/* <Sample/> */}
   </div>
 )
